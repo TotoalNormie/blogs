d60093f64f469bea7828d8ca58df993d5f368e88
Total Normie <109313344+TotoalNormie@users.noreply.github.com>
2023-11-03 17:12:49 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <title>ABLOGUS</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <header>ABLOGUS</header>
+    <main>
+        <form method="post">
+            <input type="title">
+            <textarea name="description" cols="30" rows="10"></textarea>
+            <button>pievienot <i class="fa-solid fa-thumbs-up"></i></button>
+        </form>
+        <div class="wrapper">
+            <div class="blog">
+                <h2>title</h2>
+                <p>descriptoion</p>
+            </div>
+        </div>
+    </main>
+</body>
+</html>
